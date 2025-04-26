ba570fb658e2cb3c17221208d3e3e03cd4adcb5e
CrisPassos <cristiana.pazos@gmail.com>
2025-04-26 19:59:00 +0100

    add form

--- dachser/src/app/features/calculate-profit/calculate-profit.component.ts
+++ dachser/src/app/features/calculate-profit/calculate-profit.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { DataTableComponent } from '../../shared/data-table/data-table.component';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-calculate-profit',
-  imports: [DataTableComponent],
+  imports: [DataTableComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule],
   templateUrl: './calculate-profit.component.html',
   styleUrl: './calculate-profit.component.scss'
 })
@@ -20,5 +28,24 @@ export class CalculateProfitComponent {
     { id: "0001", income: "1000", costs: "200", profit: "800" },
     { id: "0002", income: "500", costs: "900", profit: "-400" },
   ];
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      shipmentId: ['', Validators.required],
+      income: ['', Validators.required],
+      cost: ['', Validators.required],
+      additionalCost: [''],
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Formulário enviado:', this.form.value);
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 
 }
